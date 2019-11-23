54bc03d45923aa6ab2320cb56de0eccdb2ce188e
SangBeomLim <sblim731@gmail.com>
2019-11-24 00:14:16 +0900

    [추가] 파이썬 기초 문법 스터디 (자료형) - 리스트 함수 lesson03

--- pylesson/src/lesson03.py
+++ pylesson/src/lesson03.py
@@ -72,3 +72,62 @@ print(a)
 
 #리스트 관련 함수
 #1. 리스트에 요소 추가 (append)
+a = [1,2,3,4]
+a.append(5) #요소 추가
+print(a)
+a.append([2,3])
+print(a)
+
+#2. 리스트 정렬 (sort)
+a = [1,5,6,7,3,2,4,3]
+print(a)
+a.sort()
+print(a)
+
+#3. 리스트 뒤집기 (reverse)
+a = [1,4,3,2,6]
+a.reverse()
+print(a)
+
+#4. 위치 반환 (index) 리스트의 인덱스 넘버 반환
+a = [1,2,3,4,5]
+print(a.index(1)) #0
+print(a.index(4)) #3
+#print(a.index(7) #error message
+
+#5. 리스트에 요소 삽입 (insert)
+a = [1,2,3,4]
+a.insert(0, 4) #a[0]위치에 4 삽입 [4,1,2,3,4]
+print(a)
+a.insert(3, 7) #a[3]위치에 7삽입 결과 : [4,1,2,7,3,4]
+print(a)
+
+#6. 리스트에서 삭제 (remove) remove(x) x가 가장 먼저나오는 리스트 요소 삭제
+a.remove(7)
+print(a)
+a.remove(4)
+print(a)
+
+#7. 리스트 요소 꺼내기 (pop) 리스트에서 가장 마지막 요소 꺼내고 삭제하기
+a = [1,2,3,4]
+a.pop()
+print(a)
+a.pop()
+print(a)
+a.pop()
+print(a)
+a.pop()
+print(a)
+
+#8. 리스트에 포함된 요소 x의 개수 카운트 (count)
+a = [1,2,1,2,1,1,1,7]
+print(a.count(1))   #요소 1의 개수 세기
+print(a.count(2))   #요소 2의 개수 세기
+
+#9. 리스트 확장
+a = [1,2,3]
+a.extend([4,5])
+print(a)
+b = [7,8]
+a.extend(b)
+print(a)
